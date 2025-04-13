a676c851b96f946415d0887cd9530e67cfde2e3f
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-04-13 01:19:31 -0400

    Update translate_vortex.rb

--- Researchers/translate_vortex.rb
+++ Researchers/translate_vortex.rb
@@ -56,10 +56,45 @@ translate = {
   "Ana Tsuchi Tesutosujettos de le souffle"       => "A heavenly test subject whom controls air magic.",
   "Ana Tsuchi Homard Au Cochon de le noye"        => "A male lobster from the heavens whom drowned in a previous life as a privateer.",
   "Ana Tsuchi Ursinehomme de les abandonnes"      => "A bear man from the heavens abandoned as a cub.",
-  "Anos Tenchi Ursinehomme de le fissure"         => "A bear man from heaven whom controls Earthquakes."
-  "Ana Tsuchi Homard Au Cochon de la noyee"       => "A female lobster pig from the heaven whom drowned in a previous life as a privateer."
+  "Anos Tenchi Ursinehomme de le fissure"         => "A bear man from heaven whom controls Earthquakes.",
+  "Ana Tsuchi Homard Au Cochon de la noyee"       => "A female lobster pig from the heaven whom drowned in a previous life as a privateer.",
+  "Ana Tsuchi Tesutosujettos de la noyee"         => "A female test subject from the heavens whom drowned.",
+
+  ## Third Section
+  "Anos Tenchi Tesutosujettos de le fissure"      => "A test subject with unknown gender from heaven whom controls Earthquakes.", 
+  "Ana Shi Tesutosujettos de le fissure"          => "A deadly test subject whom controls Earthquakes.",
+  "Anos Tenchi Tesutosujettos de les corrompue"   => "A test subject whom controls Earth magic subjected to corrupting influences.",
+  "Ana Shi Homard Au Cochon de le recupere"       => "A deadly lobster pig whom recovered from a previous battle.",
+  "Ana Shi Homard Au Cochon de le noye"           => "A deadly lobster pig whom is the reincarnated spirit of a drowned privateer",
+  "Anos Tenchi Tesutosujettos de la noyee"        => "A test subject from the heavens whom drowned at sea,",
+  "Anos Kaiyo Ursinehomme de le fissure"          => "An aetherial bear man whom controls Earth quakes.",
+  "Ana Tsuchi Tesutosujettos de le fissure"       => "A female test subject from heaven whom controls Earthquakes.",
+  "Anos Ea Homard Au Cochon de le fissure"        => "An airborn lobster pig whom controls Earthquakes.",
+  "Anoa Shi Ursinehomme de le noyee"              => "A deadly bear man whom drowned at sea.",
+  "Anos Ea Usinehomme de la noyee"                => "An airborn bear nab whom drowned at sea,",
+  "Anos Denki Homard Au Cochon de le noye"        => "An electrically charged lobster pig whom drowned at sea.",
 }
 
-print "What would you like to translate? >> "; trans = gets.chomp
+#print "What would you like to translate? >> "; trans = gets.chomp
 
-puts translate[trans]
+#puts translate[trans]
+
+enemies_set = File.readlines("_enemynames/enemies.txt")
+
+size_limit = enemies_set.size.to_i
+
+index = 0
+
+size_limit.times do
+  #begin
+    trans = enemies_set[index].to_s.strip
+
+    print trans + " "
+    print translate[trans]       
+    puts " "
+
+    index = index + 1
+  #rescue
+    #puts "Line being read properly as a string."
+  #end
+end
