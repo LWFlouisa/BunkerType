a0080ca4819838bf96a328851e5040611e641543
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-04-12 23:44:58 -0400

    Update translate_vortex.rb

--- Researchers/translate_vortex.rb
+++ Researchers/translate_vortex.rb
@@ -1,19 +1,22 @@
 translate = {
-  "Anu Ea"  => "An airborn",
-  "Ana Ea"  => "An airborn",
-  "Anos Ea" => "An airborn",
-
-  "Anu Denki"  => "An electric",
-  "Ana Denki"  => "An electric",
+  "Anu Ea"      => "An airborn",
+  "Ana Ea"      => "An airborn",
+  "Anos Ea"     => "An airborn",
+  "Anu Denki"   => "An electric",
+  "Ana Denki"   => "An electric",
   "Anos Denki"  => "An electric",
-
-  "Anu Kaiyo"  => "An aetherial",
-  "Ana Kaiyo"  => "An aetherial",
-  "Anos Kaiyo" => "An aetherial",
-
+  "Anu Kaiyo"   => "An aetherial",
+  "Ana Kaiyo"   => "An aetherial",
+  "Anos Kaiyo"  => "An aetherial",
+  "Anu Tsuchi"  => "A heavenly",
+  "Ana Tsuchi"  => "A heavenly",
   "Anos Tsuchi" => "A heavenly",
+  "Anu Tenchi"  => "An earthen",
+  "Ana Tenchi"  => "An earthen",
   "Anos Tenchi" => "An earthen",
+  "Anu Shi"     => "A deadly",
   "Ana Shi"     => "A deadly",
+  "Anos Shi"    => "A deadly",
 
   "Homard Au Cochon" => "Lobster in the pig",
   "Tesutosujettos"   => "Test Subject",
@@ -27,8 +30,24 @@ translate = {
   "de le noye"        => "of male drownings.",
   "de le fissure"     => "of Earthquakes.",
   "de les abandonnés" => "of abandonment.",
+
+  "Anos Tsuchi Tesutosujettos de les corrompue"   => "A heavenly test subject subjected to corrupting influences.",
+  "Anos Denki Ursinehomme de le carbonise"        => "A bear man with electric and fire magic ability.",
+  "Ana Shi Ursinehomme de le recupere"            => "A deadly bear-man recovered from battle.",
+  "Ana Tsuchi Homard Au Cochon de le soufflé"     => "A monster with a pigs face and a lobster's body whom makes souffles.",
+  "Ana Tsuchi Ursinehomme de la noyee"            => "A heavenly bear-man whom drowned at sea.",
+  "Anos Ea Ursinehomme de le noye"                => "A once airbone bear-woman whom drowned at sea.",
+  "Anos Denki Tesutosujettos de le souffle"       => "A test subject tortured by blowing electrical currents.",
+  "Anos Tsuchi Tesutosujettos de la noyee"        => "A drowned test subject from heaven.",
+  "Anos Tenchi Homard Au Cochon de la noyee"      => "A lobster pig that's the reincarnated spirit of a drowned privateer with the power of Earth.",
+  "Anos Denki Tesutosujettos de les corrompue"    => "A test subject tortured by electric currents subjected to corrupting influences.",
+  "Anos Danki Usinehomme de les abandonnes"       => "An abandoned bear-man cub with electric magic abilities.",
+  "Anos Tenchi Ursinehomme de les corrompue"      => "A bear-man from heaven subjected to corrupting influences.",
+  "Anos Denki Homard Au Cochon de les abandonnes" => "A lobster pig with electric magic powered abandoned as a crawling.",
 }
 
 print "What would you like to translate? >> "; trans = gets.chomp
+
+puts translate[trans]
 
 puts translate[trans]
