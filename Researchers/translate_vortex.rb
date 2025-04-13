8ea8d691d7d7d24aeffb02c20eda55b279f08c18
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-04-13 00:46:12 -0400

    Update translate_vortex.rb

--- Researchers/translate_vortex.rb
+++ Researchers/translate_vortex.rb
@@ -31,10 +31,12 @@ translate = {
   "de le fissure"     => "of Earthquakes.",
   "de les abandonnés" => "of abandonment.",
 
+  ## First section
   "Anos Tsuchi Tesutosujettos de les corrompue"   => "A heavenly test subject subjected to corrupting influences.",
   "Anos Denki Ursinehomme de le carbonise"        => "A bear man with electric and fire magic ability.",
   "Ana Shi Ursinehomme de le recupere"            => "A deadly bear-man recovered from battle.",
-  "Ana Tsuchi Homard Au Cochon de le soufflé"     => "A monster with a pigs face and a lobster's body whom makes souffles.",
+  "Ana Tsuchi Homard Au Cochon de le souffle"     => "A monster with a pigs face and a lobster's body whom makes souffles.",
+
   "Ana Tsuchi Ursinehomme de la noyee"            => "A heavenly bear-man whom drowned at sea.",
   "Anos Ea Ursinehomme de le noye"                => "A once airbone bear-woman whom drowned at sea.",
   "Anos Denki Tesutosujettos de le souffle"       => "A test subject tortured by blowing electrical currents.",
@@ -44,10 +46,20 @@ translate = {
   "Anos Danki Usinehomme de les abandonnes"       => "An abandoned bear-man cub with electric magic abilities.",
   "Anos Tenchi Ursinehomme de les corrompue"      => "A bear-man from heaven subjected to corrupting influences.",
   "Anos Denki Homard Au Cochon de les abandonnes" => "A lobster pig with electric magic powered abandoned as a crawling.",
+  "Anos Kaiyo Ursinehomme de le recupere"         => "An aetherial bear-man recovered from a previous battle.",
+  "Ana Shi Tesutosujettos de le souffle"          => "A test subject whom died from an explosion.",
+
+  ## Second Section
+  "Anos Kaiyo Tesutosujettos de le carbonise"     => "A test subject from heaven whom uses fire magic.",
+  "Anos Tenchi Ursinehomme de la noyee"           => "A bear-man controlling Earth magic whom drowned at sea.",
+  "Ana Tsuchi Ursinehomme de le noye"             => "A drowned wear-bear from heaven.",
+  "Ana Tsuchi Tesutosujettos de le souffle"       => "A heavenly test subject whom controls air magic.",
+  "Ana Tsuchi Homard Au Cochon de le noye"        => "A male lobster from the heavens whom drowned in a previous life as a privateer.",
+  "Ana Tsuchi Ursinehomme de les abandonnes"      => "A bear man from the heavens abandoned as a cub.",
+  "Anos Tenchi Ursinehomme de le fissure"         => "A bear man from heaven whom controls Earthquakes."
+  "Ana Tsuchi Homard Au Cochon de la noyee"       => "A female lobster pig from the heaven whom drowned in a previous life as a privateer."
 }
 
 print "What would you like to translate? >> "; trans = gets.chomp
-
-puts translate[trans]
 
 puts translate[trans]
